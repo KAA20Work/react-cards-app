96cee9dbb0252ea4cdeb31c6189aa650e5f84c3a
Anny <102854214+KAA20Work@users.noreply.github.com>
2022-10-26 15:35:55 +0300

    translate fields with enum

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,6 +1,4 @@
-
-import React, {useEffect, useState} from "react";
-
+import { KeyObject } from 'crypto';
 import { InfoField } from '../Field/InfoField';
 import "./cardStyle.scss"
 
@@ -34,70 +32,70 @@ export const Card  = ({
     address,
     phone, 
     website, 
-    company } : 
-    {
-        name: string,
-        username: string,
-        email: string,
-        address: {
-            street: string,
-            suite: string,
-            city: string,
-            zipcode: string,
-            geo: {
-                lat: string,
-                lng: string
-            }
-        },
-        phone: string,
-        website: string,
-        company: {
-            name: string,
-            catchPhrase: string,
-            bs: string
-        }
+    company } : CardProps) => {
 
-    }) => {
         const mainInform = {name, username, email, phone, website};
 
-    return (
-        <li className='card'>
-            <a className="cardContent">
-                <div className="mainInfo">
-                    <img className="avatar" src="https://mobimg.b-cdn.net/v3/fetch/54/54bb741fd881313da79ec7d7f648fe9d.jpeg"/>
-                    {
-                        Object.entries(mainInform).map(([key, value]) => 
-                        <div> 
-                            <InfoField name={key} value={value} />
+        enum mainInformation {
+            name = "Имя", 
+            username = "Никнейм",
+            email = "Почта",
+            phone = "Телефон",
+            website = "Сайт",
+            address = "Адрес",
+            street = "Улица",
+            suite = "Дом",
+            city = "Город",
+            zipcode = "Почтовый индекс",
+            geo = "Координаты",
+            lat = "Широта",
+            lng = "Долгота",
+            catchPhrase = "Ключевая фраза",
+            bs = "Направление"
+        }
+
+        return (
+            
+            <li className='card'>
+                
+                <a className="cardContent">
+                    <div className="mainInfo">
+                        <img className="avatar" src="https://mobimg.b-cdn.net/v3/fetch/54/54bb741fd881313da79ec7d7f648fe9d.jpeg"/>
+                        
+                        {
+                            
+                            Object.entries(mainInform).map(([key, value]) => 
+                            <div>
+                                <InfoField name={Object.entries(mainInformation).find(([fkey, fvalue]) => fkey === key)?.[1]} value={value} />
+                            </div>
+                            )
+                        }
+                    </div>
+                    <div className="moreInformation">
+                        <div className="Location">
+                            {
+                                Object.entries(address).map(([key, value]) => 
+                                <div> 
+                                    <InfoField name={Object.entries(mainInformation).find(([fkey, fvalue]) => fkey === key)?.[1]}/>
+                                    {
+                                        typeof value == 'string' ? value : <div>{Object.entries(address.geo)
+                                            .map(([key, value]) => <InfoField name={Object.entries(mainInformation).find(([fkey, fvalue]) => fkey === key)?.[1]} value={value} />)}</div>
+                                    }
+                                </div>
+                                )
+                            }
                         </div>
-                        )
-                    }
-                </div>
-                <div className="moreInformation">
-                    <div className="Location">
-                        {
-                            Object.entries(address).map(([key, value]) => 
-                            <div> 
-                                <InfoField name={key}/>
-                                {
-                                    typeof value == 'string' ? value : <div>{Object.entries(address.geo)
-                                        .map(([key, value]) => <InfoField name={key} value={value} />)}</div>
-                                }
-                            </div>
-                            )
-                        }
+                        <div className="Company">
+                            {
+                                Object.entries(company).map(([key, value]) => 
+                                <div> 
+                                    <InfoField name={Object.entries(mainInformation).find(([fkey, fvalue]) => fkey === key)?.[1]} value={value}/>
+                                </div>
+                                )
+                            }
+                        </div>
                     </div>
-                    <div className="Company">
-                        {
-                            Object.entries(company).map(([key, value]) => 
-                            <div> 
-                                <InfoField name={key} value={value}/>
-                            </div>
-                            )
-                        }
-                    </div>
-                </div>
-            </a>
-        </li>
-    );
+                </a>
+            </li>
+        );
 }
